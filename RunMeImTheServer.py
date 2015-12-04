d4094eae88e2afdbc331cc0eb08d561c4b4a2b53
Max Llewellyn <max.llewellyn@gmail.com>
2015-12-04 12:16:36 -0500

    Added a little back and forth between the client and server.

--- RunMeImTheServer.py
+++ RunMeImTheServer.py
@@ -12,13 +12,29 @@ def start_server(host, port):
     while 1:
         print("Waiting for a new connection.")
         client_socket, client_address = server_socket.accept()
-        print(client_socket)
-        print(client_address)
         print("Received a new connection from %s:%s" % client_address)
         thread.start_new_thread(handel_new_conection, (client_socket, client_address))
 
 def handel_new_conection(client_socket, client_address):
     print("Time to handle the client at %s:%s!" % client_address)
+    # first we have to get the command from the client
+    while 1:
+        data = client_socket.recv(BUFFER_SIZE)
+
+        if not data:
+            print("Didn't get any data from the client, abandoning.")
+            break;
+        print("data: "+ repr(data))
+
+        # DO SOMETHING HERE TO GET CLIENT RESPONSE
+        # until then the server just flirts with the client
+        response = "Hey baby, come here often? I'll be your server today."
+        
+        print("response: "+ repr(response))
+        client_socket.send(response)
+
+    print("client closed connection :'-(. I guess no one wants to talk to me.")
+
 
 if __name__ == '__main__':
     start_server('', LISTENER_PORT)
