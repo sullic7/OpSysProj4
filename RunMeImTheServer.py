31da6a72b4b0e78678cf8d40c56dc73285ea9e0e
sullic7 <sullic7@rpi.edu>
2015-12-05 17:00:06 -0500

    Have a working STORE command!

--- RunMeImTheServer.py
+++ RunMeImTheServer.py
@@ -1,5 +1,6 @@
 import socket
 import thread
+import threading
 import string
 from SimulatedDisk import SimulatedDisk, SimulatedDiskError
 import signal
@@ -15,10 +16,10 @@ def start_server(host, port, disk):
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((host, port))
         server_socket.listen(5) # this is the number of possible queued connections
+        print("Listening on port %d" % port)
         while 1:
-            print("Waiting for a new connection.")
             client_socket, client_address = server_socket.accept()
-            print("Received a new connection from %s:%s" % client_address)
+            print("Received incoming connection from %s:%s" % client_address)
             thread.start_new_thread(handle_new_conection, (client_socket, client_address, disk))
     except KeyboardInterrupt:
         pass
@@ -28,7 +29,6 @@ def start_server(host, port, disk):
         server_socket.close()
 
 def handle_new_conection(client_socket, client_address, disk):
-    print("Time to handle the client at %s:%s!" % client_address)
     # first we have to get the command from the client
     try:
         while 1:
@@ -52,7 +52,7 @@ def handle_new_conection(client_socket, client_address, disk):
                 # this is a catch all response
                 response = "ERROR: something bad happened. I'm sorry hon."
             
-            print("response: "+ repr(response))
+            #print("response: "+ repr(response))
             client_socket.send(response)
     finally:
         print("finally closing client socket")
@@ -65,27 +65,25 @@ def parse_request_and_formulate_response(request, disk):
     # replace \n with spaces because that's only reasonable
     # then remove the trailing space
     request = request.replace("\n", " ")
-    print request
     request = request.strip()
+    current_thread = threading.currentThread().ident
+
+    print("[thread %d] Rcvd:" % current_thread),
+    print request
 
     # split the request and pass it to the proper function
     split_request = request.split()
-
-    # TO DO: get rid of \n at end of line and turn str into int
-
-    # I use a few extra cycles here to use some sexy syntax
+    split_request.append(current_thread)
     command = split_request.pop(0)
-    print("Command: %s" % command)
-    print("Rest of request:")
-    print split_request
+    # I use a few extra cycles here to use some sexy syntax
     try:
         if command == 'STORE':
-            print("Trying to store file %s size %d." % (split_request[0], int(split_request[1])))
+            split_request[1] = int(split_request[1])
             return disk.store(*split_request)
 
         if command == 'READ':
-            print("Trying to read file %s offset %d size %d." % 
-                (split_request[0], int(split_request[1]), int(split_request[2])))
+            split_request[1] = int(split_request[1])
+            split_request[2] = int(split_request[2])
             return disk.read(*split_request)
 
         if command == 'DELETE':
@@ -94,7 +92,7 @@ def parse_request_and_formulate_response(request, disk):
 
         if command == 'DIR':
             print("Listing the files in the system.")
-            return disk.dir()
+            return disk.dir(current_thread)
         # if we haven't returned by now there's an error
         return "ERROR: Invalid command\n"
 
--- SimulatedDisk.py
+++ SimulatedDisk.py
@@ -1,5 +1,6 @@
 """ This class has store, read, delete and dir methods for a simulated disk."""
 import threading
+import math
 
 
 class SimulatedDiskError(Exception):
@@ -25,33 +26,121 @@ class SimulatedDisk():
         self.n_blocks = n_blocks
         self.blocksize = blocksize
         self.lock = threading.Lock()
+        self.files_on_disk = {}
+        self.letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
         self.disk_mem = []
-        self.disk_file = open("disk.txt",'w')
-        self.disk_file.close()
+
+        disk_file = open(".storage.txt",'w')
+        disk_file.close()
         for i in range(self.size):
             self.disk_mem.append(".")
+        print("Block size is %d" % self.blocksize)
+        print("Number of blocks is %d" % self.n_blocks)
 
-    def store(self, filename, num_bytes, file_contents):
+    def store(self, filename, num_bytes, file_contents, threadID):
         """ Add the specified file to the storage server. """
         self.lock.acquire()
-        self.lock.release()
-        return "PLACEHOLDER RESULT OF STORE METHOD\n"
+        file_space = int(math.ceil(num_bytes/float(self.blocksize)))
+        space = self.get_open_disk_space()
+        if(space < file_space):
+            self.lock.release()
+            return "ERROR: INSUFFICIENT DISK SPACE\n"
+        elif filename in self.files_on_disk:
+            self.lock.release()
+            return "ERROR: FILE EXISTS\n"
+        elif(len(file_contents)>num_bytes):
+            self.lock.release()
+            return "ERROR: FILE CONTENTS LARGER THAN GIVEN BYTE SIZE\n"
+        elif(len(self.letters)==0):
+            self.lock.release()
+            return "ERROR: TOO MANY FILES TO SIMULATE\n"
+        else:
+            new_file = StoredFiles(filename, self.letters.pop(0), num_bytes, file_contents)
+            disk_file = open(".storage.txt",'a')
+            disk_file.write(filename + '\n')
+            disk_file.close()
+            self.files_on_disk[filename] = new_file
+            # TO DO: figure out what to do with file contents (store, read, and remove); make sure contents can be read as bit files
 
-    def read(self, filename, byte_offset, length):
-        """ Return a list of bytes from the specified offset """
+            clusters = self.add_file(new_file.letter, file_space)
+
+            print("[thread %d] Stored file '%c' (%d bytes; %d blocks; %d cluster)" % (threadID, new_file.letter, num_bytes, file_space, clusters))
+            self.show(threadID)
+            print("[thread %d] Sent: ACK" % threadID)
+            self.lock.release()
+            return "ACK\n"
+
+    def read(self, filename, byte_offset, length, threadID):
+        """ Return a list of bytes from the specified offset. """
         self.lock.acquire()
+        # use .readline()?
         self.lock.release()
-        return "PLACEHOLDER RESULT OF READ METHOD\n"
+        return "ACK\n"
 
-    def delete(self, filename):
+    def delete(self, filename, threadID):
         """ Delete the specified file on the server. """
         self.lock.acquire()
+        """
+        i = 0
+        while(i<self.size and self.disk_mem[i]!=process.proc_num):
+            i += 1
+        end_of_proc_mem = i+process.memory_size
+        while(i<self.size and i < end_of_proc_mem):
+            self.disk_mem[i] = "."
+            i += 1
+        """
+        self.show(threadID)
         self.lock.release()
-        return "PLACEHOLDER RESULT OF DELETE METHOD\n"
+        return "ACK\n"
 
-    def dir(self):
+    def dir(self, threadID):
         """ Return a list of the filenames in the server."""
         self.lock.acquire()
         self.lock.release()
-        return "PLACEHOLDER RESULT OF DIR METHOD\n"
+        return "ACK\n"
 
+    def show(self, threadID):
+        """ Print current disk space. """
+        print("[thread %d] Simulated Clustered Disk Space Allocation" % threadID)
+        line = '=' * 32
+        print line
+        for i in range(self.size/32):
+            print ''.join(self.disk_mem[32*i:32*(i+1)])
+        print line
+
+    def get_open_disk_space(self):
+        """ Return number of open blocks on disk. """
+        count = 0
+        for i in range(self.size):
+            if self.disk_mem[i]==".":
+                count += 1
+        return count
+
+    def add_file(self, letter, block_size):
+        """ Put file onto disk. """
+        cluster = 1
+        i = 0
+        j = 0
+
+        continuous = True
+        while(i<self.size and j<block_size):
+            if(self.disk_mem[i]=="."):
+                self.disk_mem[i] = letter
+                if not continuous:
+                    continuous = True
+                    cluster += 1
+                j+=1
+            else:
+                continuous = False
+            print continuous
+            i+=1
+        return cluster
+
+
+class StoredFiles():
+
+    def __init__(self, file_name, letter, num_bytes, contents):
+        self.name = file_name
+        self.letter = letter
+        self.num_bytes = num_bytes
+        self.contents = contents
